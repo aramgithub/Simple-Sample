ad234efed18f91d6a7d80d41a8d4bd0ab3dade2e
aram <azaerpoor@gmail.com>
2015-06-30 01:31:26 +1000

    This design pattern was added for better management of commands

---
+++ RobotSimulator/src/main/CommandFactory.java
@@ -0,0 +1,28 @@
+package main;
+
+public class CommandFactory {
+		
+	public Command getCommand(String commandName){
+    if(commandName == null){
+    return null;
+    }
+    if(commandName.equalsIgnoreCase("PLACE")){
+    	return new Place();
+         
+    }else if(commandName.equalsIgnoreCase("REPORT")){
+    	return new Report();
+         
+    }else if(commandName.equalsIgnoreCase("FORWARD")){
+    	return new Forward();
+	  
+    }else if(commandName.equalsIgnoreCase("RIGHT")){
+    	return new TurnRight();
+	  
+    }else if(commandName.equalsIgnoreCase("LEFT")){
+    	return new TurnLeft();
+	}
+	      
+    return null;
+   }
+}
+
