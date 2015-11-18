79a6a4a77e99e166f6be1a1e2852a69d1eb5e2b6
aram <azaerpoor@gmail.com>
2015-11-19 02:23:57 +1100

    Entering Data via command line became available.

--- payslip/src/main/java/simple/sample/payslip/App.java
+++ payslip/src/main/java/simple/sample/payslip/App.java
@@ -1,13 +1,24 @@
 package simple.sample.payslip;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try{
+	    	System.out.println( "Pay Slip Generator" );
+	        System.out.println( "Enter the input info >");
+	        BufferedReader input;
+			input = new BufferedReader(new InputStreamReader(System.in));     	        
+	   		String inputData = input.readLine();
+	   		PaySlip paySlip = new PaySlip();
+	   		System.out.println(paySlip.issuePaySlip(inputData));
+	    } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
